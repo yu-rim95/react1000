bf17a13c1d2dd66a63191447cb05f18dc3d4c42b
yu-rim95 <80299209+yu-rim95@users.noreply.github.com>
2021-05-25 14:11:54 +0900

    2021.5.25

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
+function formatDate(date) {
+  return date.toLocaleDateString();
 }
-function App() {
+
+function Comment(props) {
   return (
-    <div>
-      <Welcome name="yurim" />
-      <Welcome name="story" />
-      <Welcome name="Girl" />
+    <div className="comment">
+      <div className="uaer-info">
+        <img
+          className="avatar"
+          src={props.author.avatarUrl}
+          alt={props.author.name}
+        />
+        <div className="user-name">{props.author.name}</div>
+      </div>
+      <div className="comment-text">{props.text}</div>
+      <div className="comment-date">{formatDate(props.date)}</div>
     </div>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const comment = {
+  date: new Date(),
+  text: "열심히 할수록 기회는 따른다.",
+  author: {
+    name: "yutim",
+    avatarUrl:
+      "http://cyr2205.dothome.co.kr/portfolio/assets/images/aboutImg02.jpg",
+  },
+};
+
+ReactDOM.render(
+  <Comment date={comment.date} text={comment.text} author={comment.author} />,
+  document.getElementById("root")
+);
