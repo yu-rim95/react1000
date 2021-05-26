e45767fee54dbfec767ea546ce8e22b5e6220f02
yu-rim95 <80299209+yu-rim95@users.noreply.github.com>
2021-05-26 10:14:25 +0900

    2021-5-26

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//함수화 시키기
+function Avata(props) {
+  return <img src={props.user.avatarUrl} alt={props.user.name} />;
+}
+
 function formatDate(date) {
   return date.toLocaleDateString();
 }
 
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avata user={props.user} />
+      <div>{props.user.name}</div>
+    </div>
+  );
+}
+
 function Comment(props) {
   return (
-    <div className="comment">
-      <div className="uaer-info">
-        <img
-          className="avatar"
-          src={props.author.avatarUrl}
-          alt={props.author.name}
-        />
-        <div className="user-name">{props.author.name}</div>
-      </div>
-      <div className="comment-text">{props.text}</div>
-      <div className="comment-date">{formatDate(props.date)}</div>
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-text">{formatDate(props.date)}</div>
     </div>
   );
 }
@@ -26,7 +33,7 @@ const comment = {
   date: new Date(),
   text: "열심히 할수록 기회는 따른다.",
   author: {
-    name: "yutim",
+    name: "yurim",
     avatarUrl:
       "http://cyr2205.dothome.co.kr/portfolio/assets/images/aboutImg02.jpg",
   },
